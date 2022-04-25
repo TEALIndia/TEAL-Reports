93da8e80c45994fe5ad8c5a8b769084c47c849d6
Nabanita Sarkar <nabanita.sarkar1418@gmail.com>
2022-04-25 13:00:51 +0530

    pdf to image creation automated

--- report.py
+++ report.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 import typer
 import questionary as qs
 import json
+import os
+import fitz
 
 custom_style = qs.Style([
     ('qmark', 'fg:green'),
@@ -17,10 +19,21 @@ with open("choices.json") as f:
 def main():
   title: str = qs.text("Name of the report:", style=custom_style).unsafe_ask()
   link: str = qs.text("Report link:", style=custom_style).unsafe_ask()
-  preview_no = int(qs.text("No. of preview images:", style=custom_style).unsafe_ask())
   tag: str = qs.select(
       "Type of the report:", choices=choices, pointer="❯", style=custom_style
   ).unsafe_ask()
+  archive = qs.confirm("Is it archived report? ", style=custom_style).unsafe_ask()
+
+  os.mkdir(link)
+  # To get better resolution
+  zoom_x = 2.0  # horizontal zoom
+  zoom_y = 2.0  # vertical zoom
+  mat = fitz.Matrix(zoom_x, zoom_y)  # zoom factor 2 in each dimension
+
+  doc = fitz.open(link + '.pdf')  # open document
+  for page in doc:  # iterate through the pages
+    pix = page.get_pixmap(matrix=mat)  # render page to an image
+    pix.save(f"{link}/{str(page.number + 1).zfill(2)}.png")  # store image as a PNG
 
   if tag == "Something else":
     tag = qs.text("New type").ask()
@@ -28,12 +41,10 @@ def main():
     with open("choices.json", "w") as f:
       json.dump(choices, f)
 
-  archive = qs.confirm("Is it archived report? ", style=custom_style).unsafe_ask()
-
   obj = {
       "title": title,
       "link": link,
-      "preview_no": preview_no,
+      "preview_no": len(doc),
       "desc": "TEAL Analytics reports analyse quarterly  real estate activity including sale registrations, leasing activity, price trends, geographic trends and policy updates at a city and micro-market levels.",
       "type": tag,
       "archive": archive,
